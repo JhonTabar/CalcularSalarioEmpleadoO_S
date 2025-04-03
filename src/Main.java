3e26402938a64d7190d66b985b31331b0f682131
JhonTabar <tabaresjhonatan771@gmail.com>
2025-04-03 16:46:24 -0500

    Variables defined, text captured.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String NombreEmpleado, DocumentoEmpleado, DiaDescanso= "";
+        double SalarioBruto = 0;
+        double SalarioNeto = 0;
+        double DeduccionPension = 0;
+        double DeduccionSalud = 0;
+        double HorasTrabajadas = 0;
+        double ValorHoras = 0;
+        double BonificacionEmpleado = 0;
+        double ValorSalarioMinimo = 1423500;
+        double AuxilioTransporte = 200000;
+        double CantidadHorasExtras = 0;
+        double ValorHoraExtra = 0;
+        double TotalValorHorasExtras = 0;
+
+        System.out.println("Ingrese el nombre del empleado: ");
+        NombreEmpleado = sc.nextLine();
+        System.out.println("Ingrese el documento del empleado: ");
+        DocumentoEmpleado = sc.nextLine();
+        System.out.println("Ingrese el día de descanso: (Lunes-Viernes)");
+        DiaDescanso = sc.nextLine();
+
     }
 }
